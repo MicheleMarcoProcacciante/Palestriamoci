1c4283491bb0d3b99724f7617369ff6098b7f8b6
Riccardoitsrizzoli <154215263+Riccardoitsrizzoli@users.noreply.github.com>
2024-03-22 14:57:45 +0100

    Update cardsComposition.py
    modificata la classe con le caratteristiche necessarie

--- PW_20_03_24_python_flask/cardsComposition.py
+++ PW_20_03_24_python_flask/cardsComposition.py
@@ -1,22 +1,20 @@
 class CardsComposition:
-    id = None
-    id_trainingCards = None
-    id_exercises = None
+    exercise_name = None
+    date = None
     series = None
     reps = None
     loads = None
     rest = None
     duration = None
-    comment = None
+    comment_ = None
     
 
-    def __init__(self, id, id_t, id_e, s, rep, l, res, d, c):
-        self.id = id
-        self.id_trainingCards = id_t
-        self.id_exercises = id_e
-        self.series = s
+    def __init__(self, name, date, ser, rep, l, res, dur, c):
+        self.exercise_name = name
+        self.date = str(date)
+        self.series = ser
         self.reps = rep
         self.loads = l
-        self.rest= res
-        self.duration = d
-        self.comment = c
+        self.rest = res
+        self.duration= dur
+        self.comment_ = c
