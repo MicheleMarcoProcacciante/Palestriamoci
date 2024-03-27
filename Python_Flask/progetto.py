ddd0ab0568ede0589db5b7c5ed6a1335cec93314
Riccardoitsrizzoli <154215263+Riccardoitsrizzoli@users.noreply.github.com>
2024-03-27 19:25:07 +0100

    Update progetto.py
    aggiornato per permettere di vedere profilo utente
    (risolto anche problematica relativa all'ordine di visualizzazione dei dati)

--- Python_Flask/progetto.py
+++ Python_Flask/progetto.py
@@ -1,5 +1,5 @@
 from athletesInfoFull import Athletes
-from athletesInfoShared import AthletesInfoShared
+# from athletesInfoShared import AthletesInfoShared
 from trainingCards import TrainingCards
 from cardsComposition import CardsComposition
 from flask import Flask, request, render_template, redirect, url_for, session
@@ -47,6 +47,8 @@ def login():
     
     email = request.form.get('inputEmail')
     password = request.form.get('inputPassword')
+    # email = request.form['inputEmail']
+    # password = request.form['inputPassword']
 
     atleta = None
 
@@ -55,12 +57,12 @@ def login():
         cursor.execute(sql)
         row = cursor.fetchone()
 
-        atleta = Athletes (row[0],row[1],row[2],row[3],row[4],row[5])
+        atleta = Athletes (row[0],row[1],row[2],row[3],row[4],row[5])        
 
         session["id_loggeduser"] = atleta.id
 
 
-        return redirect ("/homepage")
+        return redirect ("/account")
     
     except:
         # notifica di errore
@@ -90,7 +92,7 @@ def showCardComposition():
 
     return json.dumps(allenamentiGiornata, default=vars)
 
-
+'''  per android
 @app.route('/showcards', methods = ['GET'])
 def showCard():
 
@@ -112,6 +114,64 @@ def showCard():
 
     # return json.dumps(schede, default=vars)
     return render_template("indexDELETE.html", datiScheda = scheda, nSchede = numeroschede)
+'''
+
+''' NO
+# PER SITO WEB
+@app.route('/showcards', methods = ['GET'])
+def showCard():
+
+# id utente, vede tutte schede in base a id utente. vedo tutti i card composition di tutte le schede.
+
+    # id = session["id_loggeduser"]
+    id = 1
+
+    sql = """select trainingCards.trainingCards_id, athletes.athletes_id, trainingCards.name_table, trainingCards.date_, cardsComposition.series, cardsComposition.reps, cardsComposition.loads,
+            cardsComposition.rest, cardsComposition.duration, cardsComposition.comment_
+            from trainingCards
+	        left join athletes on trainingcards.athletes_fk = athletes.athletes_id
+	        inner join cardsComposition on trainingCards.trainingCards_id = cardsComposition.trainingCards_fk
+            where athletes.athletes_id = '%s'""" % (id)
+    cursor.execute(sql)
+    cards = cursor.fetchall()
+
+    schede=[]
+
+    numeroschede = 0
+
+    for card in cards:
+        scheda = TrainingCards (card[0],card[1],card[2],card[3])
+        schede.append(scheda)
+        numeroschede = numeroschede + 1
+
+    # return json.dumps(schede, default=vars)
+    return render_template("indexDELETE.html", datiSchede = schede, nSchede = numeroschede)
+'''
+
+# PER SITO WEB
+@app.route('/showcards', methods = ['GET'])
+def showCard():
+
+# id utente, vede tutte schede in base a id utente. vedo tutti i card composition di tutte le schede.
+
+    # id = session["id_loggeduser"]
+    id = 1
+
+    sql = """"select * from TrainingCards where athletes_fk = '%s'" % (id)""" % (id)
+    cursor.execute(sql)
+    cards = cursor.fetchall()
+
+    schede=[]
+
+    numeroschede = 0
+
+    for card in cards:
+        scheda = TrainingCards (card[0],card[1],card[2],card[3])
+        schede.append(scheda)
+        numeroschede = numeroschede + 1
+
+    # return json.dumps(schede, default=vars)
+    return render_template("indexDELETE.html", datiSchede = schede, nSchede = numeroschede)
 
 
 @app.route('/register', methods = ['GET'])
@@ -136,22 +196,24 @@ def register():
 
     session["id_loggeduser"] = cursor.lastrowid
 
-    return redirect ("/homepage")
+    return redirect ("/account")
 
     #return render_template("buttare.html", datiHtmlNome = name, datiHtmlCognome = surname, datiHtmlEmail = email, datiHtmlPassword = password)
 
 
-@app.route("/homepage")
+@app.route("/account")
 def dettaglio():
     id = session["id_loggeduser"]
 
-    sql = f"select * from athletes where athletes_id = {id}"
+    sql = (f"select * from athletes where athletes_id = {id}")
     cursor.execute(sql)
     row = cursor.fetchone()
     
-    atleta = AthletesInfoShared (row[0],row[1],row[2],row[3],row[4])
+    atleta = Athletes (row[0],row[1],row[2],row[3],row[4],row[5])
 
-    return render_template("index.html", athlete = atleta)
+    # Password non si vede perchè manca nel html, decidere come implementarla
+
+    return render_template("my_account.html", athlete = atleta)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
