52f9c4c3a45667678488cd1848911c929edc4f1f
Riccardoitsrizzoli <154215263+Riccardoitsrizzoli@users.noreply.github.com>
2024-03-27 19:26:12 +0100

    Update athletesInfoFull.py
    risolta la problematica relativa all'ordine degli elementi

--- Python_Flask/athletesInfoFull.py
+++ Python_Flask/athletesInfoFull.py
@@ -6,11 +6,10 @@ class Athletes:
     surname = None
     date_of_birth = None
     
-
     def __init__(self, id, e, p, n, s, d):
         self.id = id
-        self.email = p
-        self.password = n
-        self.name = s
-        self.surname = e
+        self.email = e
+        self.password = p
+        self.name = n
+        self.surname = s
         self.date_of_birth = str(d)
