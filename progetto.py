0c6d980a9b9bb26cb626f6b5a47b7e8f1a396642
kartsevnik <kartsevnik@gmail.com>
2024-03-28 15:30:03 +0100

    fix visibility

--- progetto.py
+++ progetto.py
@@ -11,7 +11,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-connection = pymysql.connect(host="localhost", user="root", password="XCqgtecl06!", database="palestriamocidb", port=3306, autocommit=True)
+connection = pymysql.connect(host="localhost", user="root", password="root", database="palestriamocidb", port=3306, autocommit=True)
 
 cursor = connection.cursor()
 
