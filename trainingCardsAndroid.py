fa39a457d3c319c3dbae0736c8002a285b30a669
Riccardoitsrizzoli <154215263+Riccardoitsrizzoli@users.noreply.github.com>
2024-03-28 17:30:02 +0100

    Update and rename trainingCards.py to trainingCardsAndroid.py
    rinominata scheda, commentato id atleta

---
+++ trainingCardsAndroid.py
@@ -0,0 +1,12 @@
+class TrainingCardsAndroid:
+    id = None
+    # id_athletes = None
+    name = None
+    date = None
+    
+
+    def __init__(self, id, n, d):
+        self.id = id
+        # self.id_athletes = id_a
+        self.name = n
+        self.date = str(d)
